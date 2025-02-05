89e513ae2b8d716af6b3b79009055f53a53a602d
viniartur <viniciusarturvieira@hotmail.com>
2025-02-05 14:19:14 -0300

    adicionando exercicio em c

---
+++ 1000 Dias e codigo/C/exercism/3.difference-of-squares/Difference-of-Squares.h
@@ -0,0 +1,29 @@
+#ifndef DIFFERENCE_OF_SQUARES_H
+#define DIFFERENCE_OF_SQUARES_H
+
+unsigned int sum_of_squares(unsigned int number);
+unsigned int square_of_sum(unsigned int number);
+unsigned int difference_of_squares(unsigned int number);
+
+/*
+Instructions
+Find the difference between the square of the sum and the sum of the squares of the first N natural numbers.
+
+The square of the sum of the first ten natural numbers is (1 + 2 + ... + 10)² = 55² = 3025.
+
+The sum of the squares of the first ten natural numbers is 1² + 2² + ... + 10² = 385.
+
+Hence the difference between the square of the sum of the first ten natural numbers and the sum of the squares of the first ten natural numbers is 3025 - 385 = 2640.
+
+You are not expected to discover an efficient solution to this yourself from first principles; research is allowed, indeed, encouraged. Finding the best algorithm for the problem is a key skill in software engineering.
+
+PT-BR
+Encontre a diferença entre o quadrado da soma e a soma dos quadrados dos primeiros N números naturais.
+O quadrado da soma dos dez primeiros números naturais é (1 + 2 + ... + 10)² = 55² = 3025.
+A soma dos quadrados dos dez primeiros números naturais é 1² + 2² + ... + 10² = 385.
+Portanto, a diferença entre o quadrado da soma dos dez primeiros números naturais e a soma dos quadrados dos dez primeiros números naturais é 3025 - 385 = 2640.
+Não se espera que você descubra uma solução eficiente para isso sozinho a partir dos primeiros princípios; a pesquisa é permitida, na verdade, encorajada. Encontrar o melhor algoritmo para o problema é uma habilidade essencial na engenharia de software.
+
+*/
+
+#endif
